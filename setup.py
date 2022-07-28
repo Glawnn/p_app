b5ad6962561ac6b269ad988eb15716a17b49bcbb
Gael <prungael@gmail.com>
2022-07-28 22:40:18 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import find_packages, setup
+
+
+files = {
+    "long_description": "README.md",
+    "requirements": "requirements.txt",
+    "version": "VERSION",
+    "license": "LICENSE",
+}
+
+param = {}
+
+for key in files:
+    try:
+        with open(files[key]) as file:
+            param[key] = file.read()
+    except(OSError, IOError) as exc:
+        param[key] = ""
+
+param["name_project"] = os.path.basename(os.getcwd())
+
+setup(
+    name=param["name_project"],
+    description="TemplateFPP app",
+    packages=find_packages(exclude=("test", "venv",  "docs")),
+    install_requires=param["requirements"],
+    entry_points={"console_scripts": [f"{param['name_project']} = {param['name_project']}.app:main"]},
+    **param
+)
